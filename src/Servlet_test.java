0c2042740dc044ba1bea3afe16a9b52cfd03988f
SoraTrails <me876657621@126.com>
2016-06-25 22:32:24 +0800

    测试sql语句

--- src/Servlet_test.java
+++ src/Servlet_test.java
@@ -2,6 +2,8 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import org.json.simple.JSONObject;
+import java.sql.*;
+import com.mysql.jdbc.Driver;
 
 public class Servlet_test extends HttpServlet {
 	private String message;
@@ -18,6 +20,30 @@ public class Servlet_test extends HttpServlet {
 		ss.put("status", true);
 		ss.put("data", ss2);
 		out.print(ss);
+		//out.print();
+
+
+		try {
+		//	DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/fixer" +
+							"user=taotao&password=taotao666");
+
+			Statement  statement = conn.createStatement();         //??
+			out.print("ex");
+
+			conn.close();
+			statement.close();
+
+		} catch (SQLException ex) {	
+			// handle any errors
+			out.print("SQLException: " + ex.getMessage());
+			out.print("SQLState: " + ex.getSQLState());
+			out.print("VendorError: " + ex.getErrorCode());
+		}
+		catch(ClassNotFoundException ex){
+			out.print(ex);
+		}
 	}
 	public void destroy(){}
 }
